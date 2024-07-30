3d50ef93301bb639fb94f1824dd622d82f8a31be
BenjaminRC-TESCH <benjamin_rc@tesch.edu.mx>
2024-07-30 00:40:07 -0600

    change color navbar

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -1,8 +1,23 @@
-import { Component } from '@angular/core';
+import { Component, HostListener, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
   styleUrl: './home.component.css',
 })
-export class HomeComponent {}
+export class HomeComponent {
+  constructor() {}
+
+  ngOnInit(): void {}
+
+  @HostListener('window:scroll', ['$event'])
+  onWindowScroll(e: Event): void {
+    const arrow = document.querySelector('.arrow-bottom') as HTMLElement;
+
+    if (window.pageYOffset > 50) {
+      arrow.classList.add('scrolled');
+    } else {
+      arrow.classList.remove('scrolled');
+    }
+  }
+}
