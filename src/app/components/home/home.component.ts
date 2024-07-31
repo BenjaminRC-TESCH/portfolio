9e642000adb06257f9909c095467b9b729ac13ce
BenjaminRC-TESCH <benjamin_rc@tesch.edu.mx>
2024-07-30 21:31:31 -0600

    fix: header and nav

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -1,5 +1,7 @@
 import { Component, HostListener, OnInit } from '@angular/core';
 
+declare var particlesJS: any;
+
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
@@ -8,16 +10,32 @@ import { Component, HostListener, OnInit } from '@angular/core';
 export class HomeComponent {
   constructor() {}
 
-  ngOnInit(): void {}
+  ngOnInit(): void {
+    this.loadParticles();
+  }
 
   @HostListener('window:scroll', ['$event'])
   onWindowScroll(e: Event): void {
     const arrow = document.querySelector('.arrow-bottom') as HTMLElement;
+    const particles = document.querySelector('.particles-js') as HTMLElement;
+    const name = document.getElementById('.name') as HTMLElement;
 
     if (window.pageYOffset > 50) {
       arrow.classList.add('scrolled');
     } else {
       arrow.classList.remove('scrolled');
     }
+
+    if (window.pageYOffset > 50) {
+      particles.classList.add('scrolledparticles');
+    } else {
+      particles.classList.remove('scrolledparticles');
+    }
+  }
+
+  loadParticles(): void {
+    particlesJS.load('particles-js', 'assets/particles.json', function () {
+      console.log('particles.js loaded - callback');
+    });
   }
 }
